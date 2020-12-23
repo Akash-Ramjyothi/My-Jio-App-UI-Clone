e84f49057a25b770b5c4e5f45f836d51aa487c99
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2020-12-23 17:58:45 +0530

    Added First Commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './pages/main_page.dart';
+
+void main(){
+  runApp(
+      new MaterialApp(
+        title: "MyJio - Flutter Clone",
+        home: new MainPage(),
+      )
+  );
+}
